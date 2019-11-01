02a8e8fea5cd82f851cde6aa7ac9525340f57130
Hoang Tran <36368107+HoangTran0410@users.noreply.github.com>
2019-11-01 21:10:17 +0700

    remove sass, add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
-import HomePage from './pages/HomePage'
+import HomePage from './pages/HomePage';
+import CoursesPage from './pages/CoursesPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
-    <HomePage />
+    <Router>
+      <Switch>
+
+        <Route path="/">
+          <HomePage />
+        </Route>
+
+        <Route path="/courses">
+          <CoursesPage />
+        </Route>
+
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+
+      </Switch>
+    </Router>
   );
 }
 
