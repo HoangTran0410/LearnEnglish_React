89b3dfa8d266f7de59f834e3a568ba52bc79bace
Hoang Tran <36368107+HoangTran0410@users.noreply.github.com>
2019-11-01 15:31:00 +0700

    fix navbar

--- src/layouts/Main/components/Header/HeaderComponent.jsx
+++ src/layouts/Main/components/Header/HeaderComponent.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from "react";
-import { Icon, Affix } from "antd";
+import { Icon, Affix, Input } from "antd";
 import styled from "styled-components";
 
 export default class HeaderComponent extends Component {
@@ -13,26 +13,30 @@ export default class HeaderComponent extends Component {
         </div>
 
         <div className="topnav">
-          <a className="active" href="#home">
-            <Icon type="home" theme="filled" /> Trang chủ
-          </a>
-          <a href="#courses">
-            <Icon type="book" theme="filled" /> Khoá học
-          </a>
-          <a href="#footer">
-            <Icon type="mail" theme="filled" /> Liên hệ
-          </a>
-          <a href="#login">
-            <Icon type="user" /> Đăng nhập
-          </a>
-          <Affix offsetTop={0}>
-            <div className="search-courses" action="action_page.php">
-              <input type="text" placeholder="Tìm khoá học.." name="search" />
-              <button>
-                <Icon type="search" />
-              </button>
-            </div>
-          </Affix>
+          <div className="navs">
+            <a className="active" href="#home">
+              <Icon type="home" theme="filled" /> Trang chủ
+            </a>
+            <a href="#courses">
+              <Icon type="book" theme="filled" /> Khoá học
+            </a>
+            <a href="#footer">
+              <Icon type="mail" theme="filled" /> Liên hệ
+            </a>
+            <a href="#login">
+              <Icon type="user" /> Đăng nhập
+            </a>
+          </div>
+          {/* <Affix offsetTop={0}> */}
+          <div className="search-courses">
+            <Input.Search
+              placeholder="Tìm khoá học..."
+              enterButton
+              size="large"
+              onSearch={value => console.log(value)}
+            />
+          </div>
+          {/* </Affix> */}
         </div>
       </StyledHeaderContainer>
     );
@@ -48,81 +52,58 @@ const StyledHeaderContainer = styled.header`
   }
 
   .topnav {
-    overflow: hidden;
-    background-color: #247ba0;
+    display: flex;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+    background-color: #3f72af;
 
-    a {
-      float: left;
-      display: block;
-      color: white;
-      text-align: center;
-      padding: 14px 16px;
-      text-decoration: none;
-      font-size: 17px;
+    .navs {
+      display: flex;
+      flex-wrap: nowrap;
 
-      :hover {
-        background-color: #ededed;
-        color: black;
-      }
-
-      &.active {
-        background-color: #2196f3;
+      a {
+        display: block;
         color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        font-size: 17px;
+
+        :hover {
+          background-color: #dbe2ef;
+          color: #112d4e;
+        }
+
+        &.active {
+          background-color: #2196f3;
+          color: white;
+        }
       }
     }
 
     .search-courses {
-      float: right;
       margin: 5px;
-      // border-radius: 4px;
-      // background-color: #247ba0;
-
-      input[type="text"] {
-        width: 80%;
-        height: 100%;
-        padding: 10px;
-        font-size: 17px;
-        border: none;
-        background: #f1f1f1;
-        border-top-left-radius: 4px;
-        border-bottom-left-radius: 4px;
-      }
-
-      /* Style the submit button */
-      button {
-        width: 20%;
-        height: 100%;
-        padding: 9px;
-        background: #2196f3;
-        color: white;
-        font-size: 17px;
-        border: none;
-        cursor: pointer;
-        border-top-right-radius: 4px;
-        border-bottom-right-radius: 4px;
-
-        :hover {
-          background: #0b7dda;
-        }
-      }
     }
   }
 
   @media screen and (max-width: 700px) {
     .topnav {
-      > a,
-      .search-courses {
-        float: none;
-        display: block;
-        text-align: left;
-        width: 100%;
-        margin: 0;
-        padding: 14px;
+      flex-direction: column;
+      overflow: auto;
+
+      .navs {
+        flex-direction: column;
+        a {
+          display: block;
+          text-align: left;
+          margin: 0;
+          padding: 14px;
+        }
       }
 
       .search-courses {
-        padding: 8px;
-        background-color: #247ba0;
+        padding: 10px;
+        margin: 0;
       }
     }
   }
