61ee3bb7be82ff5ce3d626cb2ddd013ff66bae10
Hoang Tran <36368107+HoangTran0410@users.noreply.github.com>
2019-11-04 22:23:06 +0700

    Update LoginPage.js

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-
-import MainLayout from '../layouts/Main';
+import { Row, Col } from 'antd';
 
 export default class LoginPage extends Component {
   render() {
     return (
-      <MainLayout>
-        Login Page
-      </MainLayout>
+      <div style={{ margin: 30 }}>
+        <Row type="flex" justify="center">
+          <Col xs={24} sm={4} md={6} lg={8} xl={10}>
+            Col
+          </Col>
+          <Col xs={12} sm={16} md={12} lg={8} xl={4}>
+            Col
+          </Col>
+          <Col xs={12} sm={4} md={6} lg={8} xl={10}>
+            Col
+          </Col>
+        </Row>
+      </div>
     )
   }
 }
