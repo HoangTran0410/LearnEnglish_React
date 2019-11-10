92a74ff8e61b2f486857cbb3ea1586f5e07e86b4
Hoang Tran <36368107+HoangTran0410@users.noreply.github.com>
2019-11-10 12:28:03 +0700

    Create fakedata.js

---
+++ src/utils/fakedata.js
@@ -0,0 +1,40 @@
+export const courses = [
+  {
+    image: require("../assets/images/courses/course_1.jpg"),
+    title: "Software Training",
+    teacher: "Mr. John Taylor",
+    overview:
+      "Lorem ipsum dolor sit amet, consectetur adipi elitsed do eiusmod tempor",
+    students_count: 20,
+    rating: 5,
+    videos_count: 5
+  },
+  {
+    image: require("../assets/images/courses/course_2.jpg"),
+    title: "Developing Mobile Apps",
+    teacher: "Ms. Lucis",
+    overview:
+      "Lorem ipsum dolor sit amet, consectetur adipi elitsed do eiusmod tempor",
+    students_count: 73,
+    rating: 5,
+    videos_count: 12
+  },
+  {
+    image: require("../assets/images/courses/course_3.jpg"),
+    title: "Starting a Startup",
+    teacher: "Mr. Charles",
+    overview:
+      "Lorem ipsum dolor sit amet, consectetur adipi elitsed do eiusmod tempor",
+    students_count: 20,
+    rating: 4,
+    videos_count: 7
+  }
+];
+
+export const categories = [
+  "Art & Design",
+  "Business",
+  "IT & Software",
+  "Languages",
+  "Programming"
+]
