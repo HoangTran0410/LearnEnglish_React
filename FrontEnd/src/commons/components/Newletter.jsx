d9021668d92ae84e4fffc55775c229693bef3e5f
Hoang Tran <36368107+HoangTran0410@users.noreply.github.com>
2019-11-17 16:29:12 +0700

    Update Newletter.jsx

--- FrontEnd/src/commons/components/Newletter.jsx
+++ FrontEnd/src/commons/components/Newletter.jsx
@@ -4,9 +4,9 @@ export default function Newletter() {
   return (
     // <!-- Newsletter -->
 
-    <div class="newsletter">
+    <div className="newsletter">
       <div
-        class="newsletter_background parallax-window"
+        className="newsletter_background parallax-window"
         // data-parallax="scroll"
         // data-image-src="images/newsletter.jpg"
         // data-speed="0.8"
@@ -15,32 +15,34 @@ export default function Newletter() {
             "url(" + require("../../assets/images/footer/newsletter.jpg") + ")"
         }}
       ></div>
-      <div class="container">
-        <div class="row">
-          <div class="col">
-            <div class="newsletter_container d-flex flex-lg-row flex-column align-items-center justify-content-start">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="newsletter_container d-flex flex-lg-row flex-column align-items-center justify-content-start">
               {/* <!-- Newsletter Content --> */}
-              <div class="newsletter_content text-lg-left text-center">
-                <div class="newsletter_title">sign up for news and offers</div>
-                <div class="newsletter_subtitle">
+              <div className="newsletter_content text-lg-left text-center">
+                <div className="newsletter_title">
+                  sign up for news and offers
+                </div>
+                <div className="newsletter_subtitle">
                   Subcribe to lastest smartphones news & great deals we offer
                 </div>
               </div>
 
               {/* <!-- Newsletter Form --> */}
-              <div class="newsletter_form_container ml-lg-auto">
+              <div className="newsletter_form_container ml-lg-auto">
                 <form
                   action="#"
                   id="newsletter_form"
-                  class="newsletter_form d-flex flex-row align-items-center justify-content-center"
+                  className="newsletter_form d-flex flex-row align-items-center justify-content-center"
                 >
                   <input
                     type="email"
-                    class="newsletter_input"
+                    className="newsletter_input"
                     placeholder="Your Email"
                     required="required"
                   />
-                  <button type="submit" class="newsletter_button">
+                  <button type="submit" className="newsletter_button">
                     subscribe
                   </button>
                 </form>
