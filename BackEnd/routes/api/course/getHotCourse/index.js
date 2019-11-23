eed4f2bcbc09e302ae1d63c0710a4ac89dbe6d11
Tran Minh Hieu <tranminhhieu739@gmail.com>
2019-11-24 01:29:33 +0700

    Update this api use module mysql

--- BackEnd/routes/api/course/getHotCourse/index.js
+++ BackEnd/routes/api/course/getHotCourse/index.js
@@ -3,8 +3,8 @@ var getHotCourse = express.Router();
 
 var util = require('../../../../Module/Util/util');
 var connection = util.connectMysql(); // Get connection from server my sql 
-
-getHotCourse.get('/getHotCourse', (req, res, next) => {//Not parameter
+var mysql = require('../../../../Module/Mysql/getData/index')
+getHotCourse.get('/getHotCourse',async(req, res, next) => {//Not parameter
 
     let sql = 'SELECT *,COUNT(re.ID) as TotalMember FROM course co JOIN record re ON co.ID = re.CourseID'+
     ' GROUP BY co.ID'+
@@ -12,20 +12,11 @@ getHotCourse.get('/getHotCourse', (req, res, next) => {//Not parameter
     ' LIMIT 0,10';    
 
     //Begin get hot course
-    connection.query(sql, function (error, results, fields) {
-        if(error){
-            res.send({
-                status : 0
-            });
-        }
-        else{
-            res.send({
-                listHotCourse: results,
-                status : 1
-            });
-        }
-
-    });
+    let result = await mysql.getData(sql);
+    
+    res.send({
+        listHotCourse : result
+    })
     //End get hot course
 });
 
